75c8052874a753926cdcacd29acf09a017787836
MatheusRamosDeLima <ramosde.lima.matheus@escola.pr.gov.br>
2023-09-18 17:02:03 -0300

    Adição de código php ao index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,27 @@
+<?php 
+    $paginas = [
+        'home' => '<h1>Página home, seja bem vindo</h1>',
+        'doces' => '<h1>Página dos doces, escolha o que você desejar</h1>'
+    ];
+?>
+
+<?php
+    $getParamethers = configurateGet($_GET);
+    $page = $getParamethers['page'];
+    $pageContent = $paginas[$page];
+
+    function configurateGet($get) {
+        if (homeCase($get)) {
+            $get = ['page' => 'home'];
+        }
+        return $get;
+    }
+
+    function homeCase($get) {
+        return $get == [] || $get['page'] == '';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,9 +32,19 @@
 <body>
     <header>
         <h1>MIA</h1>
+        <nav>
+            <?php 
+                foreach ($paginas as $link => $value) {
+                    echo "<a href='?page=$link'>$link</a>";
+                }
+            ?>
+        </nav>
     </header>
     <main>
         <p>Teste site estático usando php</p>
+        <?php
+            echo $pageContent;
+        ?>
     </main>
     <footer>
         <p>&COPY; MatheusRamosDeLima 2023</p>
