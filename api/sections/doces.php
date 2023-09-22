7e69da0036a7c6c87d17c1884bac724047c64d8c
MatheusRamosDeLima <ramosde.lima.matheus@escola.pr.gov.br>
2023-09-22 11:55:33 -0300

    Adição do conteúdo do doces.php

--- api/sections/doces.php
+++ api/sections/doces.php
@@ -1,1 +1,39 @@
+<?php 
+    class Product {
+        public function __construct(string $name, string $price, string $img, string $description) {
+            $this->name = $name;
+            $this->price = $price;
+            $this->img = $img;
+            $this->description = $description;
+        }
+    };
+
+    $description = 'O doce mais queridinho do brasil com uma nova fórmula';
+
+    $brigadeiro = new Product('Brigadeiro', '5,30', 'img/brigadeiro.png', $description);
+    $beijinho = new Product('Beijinho', '4,70', 'img/beijinho.png', $description);
+
+    $products = [
+        'brigadeiro' => $brigadeiro,
+        'beijinho' => $beijinho,
+    ];
+?>
+
 <h1>Escolha o doce!</h1>
+<section id="search">
+    
+</section>
+<section id="products">
+    <?php 
+        foreach ($products as $p => $info) {
+            echo "
+                <div id='$p'>
+                    <img src='{$info->img}'>
+                    <h2>{$info->name}</h2>
+                    <p>{$info->price}</p>
+                    <p>{$info->description}</p>
+                </div>
+            ";
+        }
+    ?>
+</section>
