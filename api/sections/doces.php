4eb1cfec5b17dc721e7de5bc4e926ad70e847f45
MatheusRamosDeLima <ramosde.lima.matheus@escola.pr.gov.br>
2023-09-24 22:07:17 -0300

    Mudanças abaixo:
    Mudança do tipo string do price para float
    Criação de arquivo json contendo os produtos
    Verificação da página doces: doces (doces-main) e doces/info (doces-info)
    Conteúdo do doces.php para o arquivo doces-main.php

--- api/sections/doces.php
+++ api/sections/doces.php
@@ -1,10 +1,10 @@
 <?php 
     class Product {
         public string $name;
-        public string $price;
+        public float $price;
         public string $img;
         public string $description;
-        public function __construct(string $name, string $price, string $img, string $description) {
+        public function __construct(string $name, float $price, string $img, string $description) {
             $this->name = $name;
             $this->price = $price;
             $this->img = $img;
@@ -12,94 +12,28 @@
         }
     };
 
-    $brigadeiro = new Product('Brigadeiro', '5,30', 'img/brigadeiro.png', 'O doce mais famoso e o mais querido de todo o Brasil.');
-    $beijinho = new Product('Beijinho', '4,70', 'img/beijinho.png', '');
-    $bolodepote = new Product('Bolo de pote', '10,20', 'img/bolodepote.png', 'O melhor bolo de pote');
+    $brigadeiro = new Product('Brigadeiro', 5.30, 'img/brigadeiro.png', 'O doce mais famoso e o mais querido de todo o Brasil.');
+    $beijinho = new Product('Beijinho', 4, 'img/beijinho.png', '');
+    $bolodepote = new Product('Bolo de pote', 10.20, 'img/bolodepote.png', 'O melhor bolo de pote');
 
     $products = [
         'brigadeiro' => $brigadeiro,
         'beijinho' => $beijinho,
         'bolodepote' => $bolodepote,
     ];
+
+    $jsonProducts = json_encode($products);
+    file_put_contents("products.json", $jsonProducts);
 ?>
 
-<h1 class="page-title">Escolha o doce!</h1>
-<section id="search">
-    
-</section>
-<section id="products">
-    <?php 
-        foreach ($products as $p => $info) {
-            echo "
-                <div id='$p' class='product'>
-                    <img src='{$info->img}'>
-                    <div class='info'>
-                        <h2 class='product-title'>{$info->name}</h2>
-                        <p class='product-price'>R$ ".$info->price."</p>
-                        <p class='product-description'>{$info->description}</p>
-                    </div>
-                </div>
-            ";
-        }
-    ?>
-</section>
-
-<style>
-    #products {
-        display: grid;
-        grid-template-columns: 200px;
-        gap: 100px;
-    	width: 200px;
-        margin: 20px auto;
+<?php 
+    $url = $_SERVER["REQUEST_URI"];
+    function selectDocesPage($url) {
+        $url = trim($url, '/');
+        if ($url ==='programas/mia/index.php/doces') return 'doces-main';
+        else if (strpos($url, 'programas/mia/index.php/doces/info') === 0) return 'doces-info';
     }
+    $pageDoces = selectDocesPage($url);
+?>
 
-    .product {
-        width: 200px;
-        height: 300px;
-        display: flex;
-        flex-direction: column;
-        align-items: start;
-        justify-content: center;
-        border: 1px solid black;
-    }
-
-    .product img {
-        width: 100%;
-        height: 190px;
-    }
-
-    .product .info {
-        width: 100%;
-        height: 110px;
-        margin: auto;
-        gap: 5px;
-    }
-
-    .product-title {
-        font-size: 25px;
-    }
-
-    .product-price {
-        font-size: 20px;
-    }
-
-    .product-description {
-        font-size: 16px;
-    }
-
-    @media only screen and (min-width: 600px) {
-        #products {
-            grid-template-columns: 200px 200px;
-            gap: 100px;
-            width: 500px;
-        }
-    }
-
-    @media only screen and (min-width: 900px) {
-        #products {
-            grid-template-columns: 200px 200px 200px;
-            gap: 100px;
-            width: 800px;
-        }
-    }
-</style>
+<?php include_once("sections/doces/$pageDoces.php"); ?>
---
+++ api/sections/doces/doces-main.php
@@ -0,0 +1,80 @@
+<h1 class="page-title">Escolha o doce!</h1>
+<section id="search">
+    
+</section>
+<section id="products">
+    <?php 
+        foreach ($products as $p => $info) {
+            echo "
+                <div id='$p' class='product'>
+                    <img src='{$info->img}'>
+                    <div class='info'>
+                        <h2 class='product-title'>{$info->name}</h2>
+                        <p class='product-price'>R$ ".$info->price."</p>
+                        <p class='product-description'>{$info->description}</p>
+                    </div>
+                </div>
+            ";
+        }
+    ?>
+</section>
+
+<style>
+    #products {
+        display: grid;
+        grid-template-columns: 200px;
+        gap: 100px;
+    	width: 200px;
+        margin: 20px auto;
+    }
+
+    .product {
+        width: 200px;
+        height: 300px;
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        justify-content: center;
+        border: 1px solid black;
+    }
+
+    .product img {
+        width: 100%;
+        height: 190px;
+    }
+
+    .product .info {
+        width: 100%;
+        height: 110px;
+        margin: auto;
+        gap: 5px;
+    }
+
+    .product-title {
+        font-size: 25px;
+    }
+
+    .product-price {
+        font-size: 20px;
+    }
+
+    .product-description {
+        font-size: 16px;
+    }
+
+    @media only screen and (min-width: 600px) {
+        #products {
+            grid-template-columns: 200px 200px;
+            gap: 100px;
+            width: 500px;
+        }
+    }
+
+    @media only screen and (min-width: 900px) {
+        #products {
+            grid-template-columns: 200px 200px 200px;
+            gap: 100px;
+            width: 800px;
+        }
+    }
+</style>
