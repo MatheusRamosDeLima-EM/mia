fb5a602aa02d7fb76fdb5a9dd458939631291ac8
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-07 11:50:44 -0300

    Adição de mais estilo e responsividade ao error.php

--- api/sections/error.php
+++ api/sections/error.php
@@ -4,6 +4,21 @@
     .error-msg {
         font-size: 18px;
         font-family: Arial, Helvetica, sans-serif;
-        margin-inline: 25px;
+        margin: auto;
+        width: 95%;
+    }
+
+    @media only screen and (min-width: 700px) {
+        .error-msg {
+            margin-inline: 10px;
+            width: 80%;
+        }
+    }
+    
+    @media only screen and (min-width: 700px) {
+        .error-msg {
+            margin-inline: 25px;
+            width: 60%;
+        }
     }
 </style>
