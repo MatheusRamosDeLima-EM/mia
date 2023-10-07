1409799e7a37d91b53aa47eee9f8561c0409ae8b
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-07 11:47:12 -0300

    Adição de estilo ao error.php

--- api/sections/error.php
+++ api/sections/error.php
@@ -1,2 +1,9 @@
 <h1>Algo deu errado!</h1>
-<p>Provavelmente a página que você está querendo acessar não existe. Caso isso seja algum erro, e queira nos reportar sobre isso, nos <a href="contato.php">envie um e-mail</a>.</p>
+<p class="error-msg">Provavelmente a página que você está tentando acessar não existe. Caso isso seja algum erro, e queira nos reportar sobre isso, nos <a href="contato.php">envie um e-mail</a>.</p>
+<style>
+    .error-msg {
+        font-size: 18px;
+        font-family: Arial, Helvetica, sans-serif;
+        margin-inline: 25px;
+    }
+</style>
