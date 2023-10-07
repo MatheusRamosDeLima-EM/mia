0d0114531225c456a9627c12a3459a7df073b544
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-07 15:52:52 -0300

    Correção - adição do estilo base ao error.php

--- api/sections/error.php
+++ api/sections/error.php
@@ -1,28 +1,88 @@
-<h1 id="error-title">Algo deu errado...</h1>
-<p class="error-text">Provavelmente a página que você está tentando acessar não existe. <br> Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
+<div id="error-content">
+    <h1 id="error-title">Puts... Algo deu errado!</h1>
+    <div id="error-text">
+        <p>Provavelmente a página que você está tentando acessar não existe.</p>
+        <div id="see-products">
+            <p>Para voltar a ver os nossos produtos:</p>
+            <a href="/doces" id="btn-see-products">Ver produtos</a>
+        </div>
+        <p style="font-size: 18px">Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
+    </div>
+</div>
+<img src="" alt="" id="error-img">
 <style>
-    #error-title {
-        font-size: 50px;
-
+    main {
+        min-height: calc(100vh - 2 * 64px);
+        margin: 64px auto;
+        margin-bottom: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 100px;
     }
 
-    .error-text {
-        font-size: 18px;
-        font-family: Arial, Helvetica, sans-serif;
-        margin: auto;
+    #error-content {
         width: 95%;
     }
+    
+    #error-title {
+        font-size: 50px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        margin-block: 20px;
+    }
 
-    @media only screen and (min-width: 700px) {
-        .error-text {
+    #error-text {
+        font-size: 22px;
+        font-family: Arial, Helvetica, sans-serif;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+
+    #see-products {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 10px;
+    }
+
+    #btn-see-products {
+        font-size: 25px;
+        color: black;
+        text-decoration: none;
+        background-color: var(--basic-color);
+        padding: 10px;
+        border: 1px solid black;
+    }
+    
+    #error-img {
+        width: 400px;
+        height: 300px;
+    }
+
+    @media only screen and (min-width: 500px) {
+        main {
             margin-inline: 10px;
+            flex-direction: row;
+            justify-content: space-between;
+            gap: 0;
+        }
+
+        #error-content {
             width: 80%;
         }
     }
     
     @media only screen and (min-width: 700px) {
-        .error-text {
+        main {
             margin-inline: 25px;
+        }
+
+        #error-content {
             width: 60%;
         }
     }
