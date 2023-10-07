664b8a674470e0d530bae894fe349649aed75a05
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-07 15:49:53 -0300

    Estilo base adicionado ao error.php

--- api/sections/error.php
+++ api/sections/error.php
@@ -1,7 +1,12 @@
-<h1>Algo deu errado!</h1>
-<p class="error-msg">Provavelmente a página que você está tentando acessar não existe. <br> Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
+<h1 id="error-title">Algo deu errado...</h1>
+<p class="error-text">Provavelmente a página que você está tentando acessar não existe. <br> Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
 <style>
-    .error-msg {
+    #error-title {
+        font-size: 50px;
+
+    }
+
+    .error-text {
         font-size: 18px;
         font-family: Arial, Helvetica, sans-serif;
         margin: auto;
@@ -9,14 +14,14 @@
     }
 
     @media only screen and (min-width: 700px) {
-        .error-msg {
+        .error-text {
             margin-inline: 10px;
             width: 80%;
         }
     }
     
     @media only screen and (min-width: 700px) {
-        .error-msg {
+        .error-text {
             margin-inline: 25px;
             width: 60%;
         }
