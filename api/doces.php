a013fcf9a2f434b5dea51d9f55e4f318001e5252
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-08 20:30:40 -0300

    Correção do header #doces para personalização do link dos doces

--- api/doces.php
+++ api/doces.php
@@ -1,20 +1,21 @@
 <?php
     $uri = trim($_SERVER['REQUEST_URI'], '/');
 
-    echo "
-    <style>
-        header #doce {
-            background-color: green;
-            color: pink;
-        }
-    </style>";
-
+    
     include_once 'products.php';
     
     if ($uri ==='doces') include_once 'pages/sections/doces/doces-main.php';
     else if (strpos($uri, 'doces/info?p=') === 0 && verifyGetP($products, $_GET['p'])) include_once 'pages/sections/doces/doces-info.php';
     else include_once 'pages/error.php';
-
+    
+    echo "
+    <style>
+        header #doces {
+            background-color: green;
+            color: pink;
+        }
+    </style>";
+    
     function verifyGetP(array $products, string $productId) {
         foreach ($products as $productsId => $productsAttr) {
             if ($productsId === $productId) return true;
