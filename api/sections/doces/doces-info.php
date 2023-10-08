0e2b0980d4d6cda5d52ce450c90f47da37f320b4
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-07 23:31:43 -0300

    Teste - remoção do include dos products em doces-main.php e doces-info.php

--- api/sections/doces/doces-info.php
+++ api/sections/doces/doces-info.php
@@ -1,7 +1,4 @@
 <?php
-    $file_products = realpath(dirname(__FILE__) . '/../../products.php');
-    include_once $file_products;
-
     function getProductAttributes(array $products) {
         $productId = $_GET['p'];
         return $products[$productId];
--- api/sections/doces/doces-main.php
+++ api/sections/doces/doces-main.php
@@ -4,9 +4,6 @@
 </section>
 <section id="products">
     <?php
-        $file_products = realpath(dirname(__FILE__) . '/../../products.php');
-        include_once($file_products);
-
         foreach ($products as $p => $info) {
             echo "
                 <a href='/doces/info?p=$p' class='product'>
