3fed767ed132a3ba618e88810156dc9281dabd4a
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-19 18:45:35 -0300

    Melhor arquitetura definida
    Css e Js colocados nos assets

--- api/Views/pages/sections/doces/index.php
+++ api/Views/pages/sections/doces/index.php
@@ -8,7 +8,7 @@
         foreach ($products as $productId => $product) {
             echo "
                 <a href='/doces/info/$productId' class='product'>
-                    <div style='background-image: url(../../../imgs/{$product->__get('image')});' class='img'></div>
+                    <div style='background-image: url(/midia/images/products/{$product->__get('image')});' class='img'></div>
                     <div class='info'>
                         <h2 class='product-title'>{$product->__get('name')}</h2>
                         <p class='product-description'>{$product->__get('description')}</p>
