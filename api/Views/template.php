3ec1936b97e98be2b2155752e66970ce3fc437a3
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-22 19:04:41 -0300

    Front: Remoção do arquivo hover.css para aquele código ser colocado no próprio index.css

--- api/Views/template.php
+++ api/Views/template.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/style.css">
-    <link rel="stylesheet" href="/assets/css/hover.css">
     <link rel="stylesheet" href="/assets/css/pages/<?php echo $this->getStyle() ?>.css">
     <script src="/assets/js/index.js" defer></script>
     <title>MIA - Loja de doces</title>
