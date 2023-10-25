0b8d5f51bd5fe602d321801ca19d26316df39d85
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-25 16:36:29 -0300

    Front: mudança de estilos na seção de informações dos produtos

--- api/Views/pages/sections/doces/info.php
+++ api/Views/pages/sections/doces/info.php
@@ -6,8 +6,12 @@
 <h1 id="product-title"><?php echo $product->__get('name')?></h1>
 <section id="product-info">
     <div id="product-info-main">
-        <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-img">
-        <p id="product-price">R$<?php echo $product->__get('price') ?></p>
+        <div id="product-info-main-image">
+            <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
+        </div>
+        <div id="product-info-main-text">
+            <p id="product-price">R$ <?php echo number_format($product->__get('price'), 2, ',', '.') ?></p>
+        </div>
     </div>
     <div id="product-info-more">
         <p id="product-description"><?php echo $product->__get('description') ?></p>
