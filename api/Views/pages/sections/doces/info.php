7cf52a6cf5de81cad3263fea2b7edd1319a1ba54
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-25 17:35:56 -0300

    Front: Mudança na estrutura html e estilização css respectiva

--- api/Views/pages/sections/doces/info.php
+++ api/Views/pages/sections/doces/info.php
@@ -5,9 +5,12 @@
 ?>
 <h1 id="product-title"><?php echo $product->__get('name')?></h1>
 <section id="product-info">
-    <div id="product-info-image">
-        <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
-    </div>
+    <picture id="product-info-image">
+        <div id="product-image-container">
+            <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
+        </div>
+        <figcaption>* Imagem meramente ilustrativa</figcaption>
+    </picture>
     <div id="product-info-text">
         <p id="product-description"><?php echo $product->__get('description') ?></p>
         <p id="product-price">R$ <?php echo number_format($product->__get('price'), 2, ',', '.') ?></p>
