095a3d890f7b770beb127bbfbeb9245e2817d80e
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-25 17:26:06 -0300

    Front: Mudando novamente a estrutura html e estilo da página info

--- api/Views/pages/sections/doces/info.php
+++ api/Views/pages/sections/doces/info.php
@@ -5,15 +5,11 @@
 ?>
 <h1 id="product-title"><?php echo $product->__get('name')?></h1>
 <section id="product-info">
-    <div id="product-info-main">
-        <div id="product-info-main-image">
-            <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
-        </div>
-        <div id="product-info-main-text">
-            <p id="product-price">R$ <?php echo number_format($product->__get('price'), 2, ',', '.') ?></p>
-        </div>
+    <div id="product-info-image">
+        <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
     </div>
-    <div id="product-info-more">
+    <div id="product-info-text">
         <p id="product-description"><?php echo $product->__get('description') ?></p>
+        <p id="product-price">R$ <?php echo number_format($product->__get('price'), 2, ',', '.') ?></p>
     </div>
 </section>
