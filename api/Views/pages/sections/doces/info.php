5d37ab2ef82ef4ea7f112ff680a6da8f8e5463a0
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-26 22:02:48 -0300

    Front: Mudança completa no design da página dos detalhes dos produtos

--- api/Views/pages/sections/doces/info.php
+++ api/Views/pages/sections/doces/info.php
@@ -3,15 +3,15 @@
     $productId = $this->getData()['productId'];
     $product = $products[$productId];
 ?>
-<h1 id="product-title"><?php echo $product->__get('name')?></h1>
-<section id="product-info">
-    <picture id="product-info-image">
-        <div id="product-image-container">
+<section id="product">
+    <picture id="product-sect-image">
+        <div id="product-image-cnt">
             <img src="/midia/images/products/<?php echo $product->__get('image') ?>" id="product-image">
         </div>
         <figcaption>* Imagem meramente ilustrativa</figcaption>
     </picture>
-    <div id="product-info-text">
+    <div id="product-sect-text">
+        <h1 id="product-title"><?php echo $product->__get('name')?></h1>
         <p id="product-description"><?php echo $product->__get('description') ?></p>
         <p id="product-price">R$ <?php echo number_format($product->__get('price'), 2, ',', '.') ?></p>
     </div>
