58575d44e5255b359279d85d7699ec20d9ad9e0c
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-10-29 23:33:39 -0300

    Front: Mudança de estrutura html e css da página de erro

--- api/Views/pages/error.php
+++ api/Views/pages/error.php
@@ -1,12 +1,14 @@
-<div id="error-content">
-    <h1 id="error-title">Puts... Algo deu errado!</h1>
-    <div id="error-text">
-        <p>Provavelmente a página que você está tentando acessar não existe.</p>
-        <div id="see-products">
-            <p>Para voltar a ver os nossos produtos:</p>
-            <a href="/doces" id="btn-see-products">Ver produtos</a>
+<section id="error-msg">
+    <div id="error-content">
+        <h1 id="error-title">Puts... Algo deu errado!</h1>
+        <div id="error-text">
+            <p>Provavelmente a página que você está tentando acessar não existe.</p>
+            <div id="see-products">
+                <p>Para voltar a ver os nossos produtos:</p>
+                <a href="/doces" id="btn-see-products">Ver produtos</a>
+            </div>
+            <p style="font-size: 18px">Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
         </div>
-        <p style="font-size: 18px">Caso isso seja um erro, favor <a href="/contato">entrar em contato</a>.</p>
     </div>
-</div>
-<img src="" alt="" id="error-img">
+    <img src="" alt="" id="error-img">
+</section>
