ee1a72677a04c8b4ade2a9926830abd2caeb1568
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-01 00:20:13 -0300

    Adição de imagem para a palha italiana, adição do produto bolo e melhora dos textos dos produtos

--- api/Models/products.php
+++ api/Models/products.php
@@ -3,26 +3,26 @@
         'brigadeiro' => new Product(
             'Brigadeiro',
             'O doce que é paixão nacional',
-            ['Tradicional' => 3, 'Leite ninho' => 4],
+            ['Tradicional' => 3, 'Branquinho' => 3,'Leite ninho' => 4],
             'brigadeiro.jpg',
             true,
-            '<p>Doce tradicional brasileiro feito com chocolate, leite condensado e manteiga. É o preferido de muitos, com seu sabor doce e cremoso.</p>'
+            '<p>O doce tradicional brasileiro agora com mais sabores e possibilidades. Mais do que um simples brigadeiro, uma explosão de emoções. É o preferido de muitos, com seu sabor doce e cremoso.</p>'
         ),
         'palha-italiana' => new Product(
             'Palha italiana',
             'O crocante que é irresistível',
             ['Tradicional' => 3 , 'Leite ninho com Oreo' => 4],
-            'palha-italiana.jpg',
+            'palha-italiana.jpeg',
             false,
-            '<p>Doce feito com biscoito, chocolate e leite condensado. É crocante e chocolatudo, com sabor irresistível.</p>'
+            '<p>Doce feito com biscoito, chocolate e leite condensado. Possui duas principais versões, uma mais saborosa que a outra. É crocante e chocolatudo, com sabor irresistível.</p>'
         ),
-        'bombom' => new Product(
-            'Bombom',
-            'O doce clássico que não pode faltar',
-            ['Chocolate' => 3.5],
-            'bombom.jpg',
+        'bolo' => new Product(
+            'Bolo',
+            'O bolo que lembra memórias de infância',
+            ['Chocolate' => 5.5],
+            'bolo-de-pote.jpg',
             true,
-            '<p>Doce feito com biscoito, chocolate e leite condensado. É crocante e chocolatudo, com sabor irresistível.</p>'
+            '<p>Bolo caseiro feito com ingredientes de qualidade e extremo cuidado com o sabor e textura. Fará você ter experiências surreais. Bolo saboroso e fofinho, ideal para aproveitar em diversos momentos e com companhia.</p>'
         )
     ];
 ?>
