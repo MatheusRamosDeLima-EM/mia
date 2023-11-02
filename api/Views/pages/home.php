39716e2e17d8027bb388cfbd9da5d33df30a5f8e
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-02 04:59:25 -0300

    Front: Mudança de estrutura html e css da apresentação da view home

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -4,4 +4,5 @@
         <p>Encontre os doces perfeitos para celebrar momentos especiais. Oferecemos uma variedade de doces deliciosos e artesanais, feitos com ingredientes de alta qualidade. Temos opções para todos os gostos e ocasiões.</p>
         <a href="/doces">Ver produtos</a>
     </div>
+    <div id="apresentation-img"></div>
 </section>
