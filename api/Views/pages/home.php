36711fe8a6b822b38e4c61da63da47e53f64ecd7
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-02 04:27:41 -0300

    Front: Adição de conteúdo a view home (sujeito a alterações e correções)

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -1,1 +1,7 @@
-<h1>Home Page</h1>
+<section id="apresentation">
+    <div id="apresentation-txt">
+        <h1>Mais do que doces, entregamos momentos</h1>
+        <p>Encontre os doces perfeitos para celebrar momentos especiais. Oferecemos uma variedade de doces deliciosos e artesanais, feitos com ingredientes de alta qualidade. Temos opções para todos os gostos e ocasiões.</p>
+        <a href="/doces">Ver produtos</a>
+    </div>
+</section>
