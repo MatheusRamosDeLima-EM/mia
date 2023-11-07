52d68af961d9f123b2b734266a14e39890d3bcb2
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-07 16:53:19 -0300

    Front: Correção de hover da página home

--- api/Views/pages/home.php
+++ api/Views/pages/home.php
@@ -2,6 +2,6 @@
     <h1>Mais do que doces, entregamos momentos</h1>
     <div id="apresentation-txt">
         <p>Encontre os doces perfeitos para celebrar momentos especiais. Oferecemos uma variedade de doces deliciosos e artesanais, feitos com ingredientes de alta qualidade. Temos opções para todos os gostos e ocasiões.</p>
-        <a href="/doces">Ver produtos</a>
+        <a href="/doces" id="btn-see-products">Ver produtos</a>
     </div>
 </section>
