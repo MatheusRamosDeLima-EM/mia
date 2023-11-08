803e471603ac27253495c3a99b94b40ac0ffbd87
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-08 15:45:55 -0300

    Front: Melhora do design da home Back: Remoção do sabor leite ninho do produto brigadeiro

--- api/Models/products.php
+++ api/Models/products.php
@@ -3,7 +3,7 @@
         'brigadeiro' => new Product(
             'Brigadeiro',
             'O doce que é paixão nacional',
-            ['Tradicional' => 3, 'Branquinho' => 3,'Leite ninho' => 4],
+            ['Tradicional' => 3, 'Branquinho' => 3],
             'brigadeiro.jpg',
             true,
             '<p>O doce tradicional brasileiro agora com mais sabores e possibilidades. Mais do que um simples brigadeiro, uma explosão de emoções. É o preferido de muitos, com seu sabor doce e cremoso.</p>'
