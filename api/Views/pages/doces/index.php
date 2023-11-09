275d43211de76d2676c43f28ada83e181d7d98b0
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-09 00:18:43 -0300

    Fullstack - modificação
    Front: Melhora da estilização da página sobre nós
    Back: Melhor organização de código da página index dos doces

--- api/Views/pages/doces/index.php
+++ api/Views/pages/doces/index.php
@@ -1,17 +1,13 @@
+<?php $products = $this->getData() ?>
 <h1>A doçura que você procura</h1>
 <section id="products">
-    <?php
-        $products = $this->data;
-        foreach ($products as $productId => $product) {
-            echo "
-                <a href='/doces/info/$productId' rel='next' class='product'>
-                    <div style='background-image: url(/midia/images/products/{$product->__get('image')});' class='img'></div>
-                    <div class='info'>
-                        <h2 class='product-title'>{$product->__get('name')}</h2>
-                        <p class='product-slogan'>{$product->__get('slogan')}</p>
-                    </div>
-                </a>
-            ";
-        }
-    ?>
+    <?php foreach ($products as $productId => $product): ?>
+    <a href="/doces/info/<?php echo $productId ?>" rel="next" class="product">
+        <div style="background-image: url(/midia/images/products/<?php echo $product->__get('image') ?>" class="img"></div>
+        <div class="info">
+            <h2 class="product-title"><?php echo $product->__get('name') ?></h2>
+            <p class="product-slogan"><?php echo $product->__get('slogan') ?></p>
+        </div>
+    </a>
+    <?php endforeach ?>
 </section>
