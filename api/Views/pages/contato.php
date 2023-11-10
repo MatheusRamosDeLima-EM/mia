737584ce976a377a88c815998127745580c67072
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-09 22:54:07 -0300

    Front: Criação do formulário de contato (sem estilo)

--- api/Views/pages/contato.php
+++ api/Views/pages/contato.php
@@ -1,1 +1,19 @@
 <h1>Nos contate</h1>
+<form action="<?= $PHP_SELF ?>" method="post">
+    <div id="container-name">
+        <label for="input-name">Nome</label>
+        <input type="text" name="name" id="input-name">
+    </div>
+    <div id="container-email">
+        <label for="input-email">Email</label>
+        <input type="email" name="email" id="input-email">
+    </div>
+    <div id="container-text">
+        <label for="input-text">Mensagem</label>
+        <textarea name="text" id="input-text" cols="30" rows="10"></textarea>
+    </div>
+    <div id="container-actions">
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Apagar">
+    </div>
+</form>
