b1a1232a1240d9036d934900cd642efecdfa5e1f
MatheusRamosDeLima-EM <ramosde.lima.matheus@escola.pr.gov.br>
2023-11-10 02:24:22 -0300

    Front: Adição de mais estilizações ao formulário da view contato

--- api/Views/pages/contato.php
+++ api/Views/pages/contato.php
@@ -1,19 +1,19 @@
 <h1>Nos contate</h1>
 <form action="/contato" method="post">
-    <div id="container-name">
+    <div class="container-input">
         <label for="input-name">Nome</label>
-        <input type="text" name="name" id="input-name">
+        <input type="text" name="name" id="input-name" placeholder="Digite o seu nome">
     </div>
-    <div id="container-email">
+    <div class="container-input">
         <label for="input-email">Email</label>
-        <input type="email" name="email" id="input-email">
+        <input type="email" name="email" id="input-email" placeholder="Digite o seu email">
     </div>
     <div id="container-text">
         <label for="input-text">Mensagem</label>
         <textarea name="text" id="input-text" cols="30" rows="10"></textarea>
     </div>
     <div id="container-actions">
-        <input type="submit" value="Enviar">
+        <input type="submit" id="submit-button" value="Enviar">
         <input type="reset" value="Apagar">
     </div>
 </form>
